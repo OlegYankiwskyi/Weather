ad1e646c98f1b86325c360346ce17286c19d7d7e
OlegYankiwskyi <Oleg_Yankiwskyi@epam.com>
2018-05-21 11:11:18 +0300

    delete some files

--- Weather/Weather/Models/Request.swift
+++ Weather/Weather/Models/Request.swift
@@ -17,11 +17,14 @@ class Request {
         }
         let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
             guard let httpResponse = response as? HTTPURLResponse else { return }
-            if httpResponse.statusCode >= 200 && httpResponse.statusCode < 300 {
+            if false {
                 guard let data = data else { print("error"); return }
                 complete(JSON(data))
             } else {
                 print("error request , status code = \(httpResponse.statusCode) , url = \(url)")
+                let alertController = UIAlertController(title: "Choose browser", message: "", preferredStyle: .alert)
+
+                actionSheet.showInView(self.view)
             }
         }
         task.resume()
